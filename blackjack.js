d39289d482580e80a91bf60b010c797511a07e54
Zivi Weinstock <zivi.weinstock@gmail.com>
2016-10-30 21:49:55 -0700

    User lodash shuffle method.

--- blackjack.js
+++ blackjack.js
@@ -1,19 +1,33 @@
 let readlineSync = require('readline-sync');
+let _ = require('lodash');
 let playerHand = [];
 let dealerHand = [];
-
+let moveChoices = ['hit', 'stay'];
 let makeDeck = require('./makeDeck')
 let deck = makeDeck.deck();
 
-function deal(deck) {
-  let cardInPlay = deck.splice(Math.floor(Math.random() * (deck.length)), 1);
-  return cardInPlay[0];
+function startGame() {
+  deck = _.shuffle(deck);
+  playerHand.push(deck.pop());
+  playerHand.push(deck.pop());
+  let score = playerHand[0].value + playerHand[1].value;
+  let move = readlineSync.keyInSelect(moveChoices, 'Your hand is: ' +
+    playerHand[0].name + ', ' + playerHand[1].name + ' your score is: ' + score +
+    ', would you like to hit or stay?');
+
+  if (move === 0) {
+    dealCard(playerHand);
+  } else if (move === 1) {
+   dealerStartGame(playerHand);
+  }
 }
 
-function startGame(deck) {
- playerHand.push(deal(deck));
- playerHand.push(deal(deck));
- 
+function dealCard(playerHand) {
+
 }
 
-startGame(deck);
+function dealerStartGame(playerHand) {
+
+}
+
+startGame();

5a78ff78f9226267cc73d9f7ba5bf09c589292c2
Zivi Weinstock <zivi.weinstock@gmail.com>
2016-10-31 20:44:28 -0700

    User play functionality.

--- blackjack.js
+++ blackjack.js
@@ -11,22 +11,62 @@ function startGame() {
   playerHand.push(deck.pop());
   playerHand.push(deck.pop());
   let score = playerHand[0].value + playerHand[1].value;
-  let move = readlineSync.keyInSelect(moveChoices, 'Your hand is: ' +
-    playerHand[0].name + ', ' + playerHand[1].name + ' your score is: ' + score +
-    ', would you like to hit or stay?');
+  if (score === 21) {
+    console.log(
+      'Your hand is: ' +
+      playerHand[0].name + ', ' + playerHand[1].name + ' your score is: ' + score +
+      'You win!'
+    );
+    return;
+  }
+  let move = readlineSync.keyInSelect(
+    moveChoices,
+    'Your hand is: ' + playerHand[0].name + ', ' + playerHand[1].name +
+    ' your score is: ' + score + ', would you like to hit or stay?');
 
   if (move === 0) {
-    dealCard(playerHand);
+    dealCard(playerHand, score);
   } else if (move === 1) {
-   dealerStartGame(playerHand);
+   dealerStartGame(playerHand, score);
   }
 }
 
-function dealCard(playerHand) {
+function dealCard(playerHand, score) {
+  let cardInPlay = deck.pop();
+  let cardString = ' Your hand is: ';
+  playerHand.push(cardInPlay);
+  score += cardInPlay.value;
 
+  for (var i = 0; i < playerHand.length; i++) {
+    cardString += (playerHand[i].name + ' ');
+  }
+
+  if (score === 21) {
+    console.log(
+      'You drew ' + cardInPlay.name + cardString +
+      '. Total 21 points. Time for the dealer to draw');
+    return dealerStartGame(score)
+  }
+  if (score > 21) {
+    console.log(
+      'You drew: ' + cardInPlay.name  + cardString + '. Your score is: ' +
+      score + ' you lose :-('
+    );
+  } else {
+    let move = readlineSync.keyInSelect(
+      moveChoices,
+      cardString + ' your score is: ' + score +
+      ', would you like to hit or stay?'
+    );
+    if (move === 0) {
+      dealCard(playerHand, score);
+    } else if (move === 1) {
+     dealerStartGame(score);
+    }
+  }
 }
 
-function dealerStartGame(playerHand) {
+function dealerStartGame(score) {
 
 }
 
